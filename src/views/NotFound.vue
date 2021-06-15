99cc053f29a10bfb2ac0e962b7fa0a10fae5dfa5
zhangqingchen <18352868150@163.com>
2021-06-15 22:30:58 +0800

    404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>404
+  <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
