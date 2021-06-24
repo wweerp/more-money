da00b70bcbe55a991e3556f6a30762851e514ad1
zhangqingchen <18352868150@163.com>
2021-06-24 22:59:04 +0800

    JS实现Types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-name: "Types"
+  name: "Types",
+  data(){
+    return {
+      type:'-' // '-'表示支出，'+'表示收入
+    }
+  },
+  methods:{
+    selectType(type) {
+      if(type !== '-' && type !== '+'){
+        throw new Error('type is unknown')
+      }
+      this.type = type
+    }
+  }
+
 }
 </script>
 
@@ -18,6 +32,7 @@ name: "Types"
   background: #C4C4C4;
   display: flex;
   font-size: 24px;
+
   > li {
     width: 50%;
     height: 64px;
@@ -25,6 +40,7 @@ name: "Types"
     justify-content: center;
     align-items: center;
     position: relative;
+
     &.selected::after {
       content: '';
       position: absolute;
